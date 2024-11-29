7a28d7bc849a5fd634f14a52dd775b73e12c8600
Asishkumar <gouda0682@gmail.com>
2024-11-29 13:21:27 +0530

    Day-1 Completed

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1" # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+  ami           = "ami-0866a3c8686eaeeba" # Specify an appropriate AMI ID
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0184a699c88df7e6e"
 }
