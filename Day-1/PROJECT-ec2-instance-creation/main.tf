32bbe45163bb6b1b8b9342bc7ac00feaed0e1932
Asishkumar <gouda0682@gmail.com>
2024-11-30 10:10:50 +0530

    Created EC2 instance with  Subnet, key_pair, tag to name instance

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,4 +6,9 @@ resource "aws_instance" "example" {
   ami           = "ami-0866a3c8686eaeeba" # Specify an appropriate AMI ID
   instance_type = "t2.micro"
   subnet_id     = "subnet-0184a699c88df7e6e"
+  key_name      = "mern-key"
+
+  tags = {
+    Name = "Terraform-Createdd-Instance" # Desired name for EC2 instance
+  }
 }
