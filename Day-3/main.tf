8a19e7be4520c5367539f90822a54284356054d3
Asishkumar <gouda0682@gmail.com>
2024-12-03 17:25:43 +0530

    Day - 3

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -3,8 +3,9 @@ provider "aws" {
 }
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  source              = "./modules/ec2_instance"
+  ami_value           = "ami-0866a3c8686eaeeba" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  subnet_id_value     = "subnet-0184a699c88df7e6e" # replace this
+  tag_value           = "Terraform-Created-Instance"
 }
--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -1,11 +1,17 @@
 variable "ami_value" {
-    description = "value for the ami"
+  description = "value for the ami"
 }
 
 variable "instance_type_value" {
-    description = "value for instance_type"
+  description = "value for instance_type"
 }
 
 variable "subnet_id_value" {
-    description = "value for the subnet_id"
+  description = "value for the subnet_id"
+}
+
+variable "tag_value" {
+  description = "Value for the Name tag"
+  type        = string
+  default     = "Default-Instance-Name"
 }
