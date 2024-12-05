c3677b9f863a923b090dc0511ce4046088a60088
Asishkumar <gouda0682@gmail.com>
2024-12-05 09:33:44 +0530

    Day 4 completed

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -2,20 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "asish" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami           = "ami-0866a3c8686eaeeba"    # change this
+  subnet_id     = "subnet-0184a699c88df7e6e" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "asish-s3-demo-xyz" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
