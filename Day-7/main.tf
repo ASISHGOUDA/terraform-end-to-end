644df6da820c2250ba9346b0bd005b6ac2672623
Asishkumar <gouda0682@gmail.com>
2024-12-10 18:50:04 +0530

    Day -7 completed

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -3,21 +3,21 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "<>:8200"
+  address          = "http://54.234.69.38:8200"
   skip_child_token = true
 
   auth_login {
     path = "auth/approle/login"
 
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id   = "fe8057f0-c78c-1c3b-5221-e62512b836c3"
+      secret_id = "8725205c-0680-aa7a-05b3-b4726a9e15d2"
     }
   }
 }
 
 data "vault_kv_secret_v2" "example" {
-  mount = "secret" // change it according to your mount
+  mount = "kv"          // change it according to your mount
   name  = "test-secret" // change it according to your secret
 }
 
@@ -26,7 +26,7 @@ resource "aws_instance" "my_instance" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    Name   = "test"
+    Secret = data.vault_kv_secret_v2.example.data["username"]
   }
 }
